f838854b2cc921cb7628d49cbc6735134818f1e1
faizal-muzakki <faizal.muzakki@gmail.com>
2018-03-22 16:42:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('68OMZuFJsIFTH0NY4hwT+AhNFXc3vuCwhCSpxLEIJqSrYF1d/1R9fzln9QkQwPDfJp0LnaaeQ49Cdw/I5HsEWmaf4T2tdQRCYTEelatKA9t9q/nUaQKRFzUeX0x9z3H2MBMvfqtFROaKFUutAqp7IgdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('ddc9210bc0da8745bd9893deccf980cf')
+line_bot_api = LineBotApi('Knp3BX1GTLB4Ebps2QNVahlonQaPsSG5zyt8s4WLIelur/0rjetXUwe6xY08RBLz+yrMqnDv3jr6IlKnH7YmDUEO+bjaTRAeV989Qf5aaLOitpDohd6krQeOr7zCgXEQnFS8iRosHS0NJxgQQVg36wdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('5e4e5ac3a9c9b0be41eb0a0971f77158')
 
 
 @app.route("/callback", methods=['POST'])
